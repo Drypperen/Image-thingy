d9a2799fc63878f939736a6bf741e5d0e81fa8c7
Drypperen <90191382+Drypperen@users.noreply.github.com>
2021-09-23 18:06:36 +0200

    Update sketch.js

--- Image_thingy_259_2021_09_22_12_01_22/sketch.js
+++ Image_thingy_259_2021_09_22_12_01_22/sketch.js
@@ -1,7 +1,37 @@
+var video;
+
 function setup() {
   createCanvas(400, 400);
+  pixelDensity(1);
+  video = createCapture(VIDEO);
+  video.size(400, 400);
 }
 
 function draw() {
   background(220);
+  
+  video.loadPixels();
+  loadPixels();
+  
+  for (var y = 0; y < video.height; y++){
+   for(var x = 0; x < video.width; x++){ 
+     var index = (video.width - x + 1 + (y * video.width)) * 4;
+     var r = video.pixls[index + 0];
+     var g = video.pixls[index + 1];     
+     var b = video.pixls[index + 2];
+     
+     var bright = (r+g+b)/3;
+     
+     var w = map(bright, 0, 255, 0, vScale);
+     
+     
+     noStroke();
+     fill(255);
+     rectMode(CENTER);
+     rect(x + vScale, y + vScale, vScale, vScale);
+   }
+  }
+  
+  updatePixels();
+  
 }
