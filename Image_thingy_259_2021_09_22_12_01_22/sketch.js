6860eae737798018ea26d3bfc582220a45652176
Drypperen <90191382+Drypperen@users.noreply.github.com>
2021-09-24 08:54:47 +0200

    Update sketch.js

--- Image_thingy_259_2021_09_22_12_01_22/sketch.js
+++ Image_thingy_259_2021_09_22_12_01_22/sketch.js
@@ -1,37 +1,45 @@
 var video;
 
+var vScale = 5;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 480);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(400, 400);
+  video.size(width / vScale, height / vScale);
 }
 
 function draw() {
-  background(220);
-  
+  background(51);
   video.loadPixels();
-  loadPixels();
-  
-  for (var y = 0; y < video.height; y++){
-   for(var x = 0; x < video.width; x++){ 
-     var index = (video.width - x + 1 + (y * video.width)) * 4;
-     var r = video.pixls[index + 0];
-     var g = video.pixls[index + 1];     
-     var b = video.pixls[index + 2];
-     
-     var bright = (r+g+b)/3;
-     
-     var w = map(bright, 0, 255, 0, vScale);
-     
-     
-     noStroke();
-     fill(255);
-     rectMode(CENTER);
-     rect(x + vScale, y + vScale, vScale, vScale);
-   }
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (video.width - x + 1 + (y * video.width)) * 4;
+      var r = video.pixels[index + 0];
+      var g = video.pixels[index + 1];
+      var b = video.pixels[index + 2];
+      var bright = (r + g + b) / 3;
+
+      if(bright > 90){
+
+        var w = map(255, 0, 255, 0, vScale);
+      noStroke();
+      fill(255);
+      rectMode(CENTER);
+      rect(x * vScale, y * vScale, w, w);
+
+      } else {
+
+        var w = map(0, 0, 255, 0, vScale);
+      noStroke();
+      fill(255);
+      rectMode(CENTER);
+      rect(x * vScale, y * vScale, w, w);
+
+      }
+
+
+    }
   }
-  
-  updatePixels();
-  
+
 }
